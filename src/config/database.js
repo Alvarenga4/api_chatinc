0acdcf091c3ece6bba419a5879443ef6739ecac9
Francisco Carlos de Alvarenga <francisco@athoslabs.com.br>
2020-03-10 11:47:48 +0000

    Resolvendo conflito de arquivos.

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,14 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 module.exports = {
-  dialect: process.env.DIALECT_DATABASE,
-  host: process.env.HOST_DATABASE,
-  username: process.env.USERNAME_DATABASE,
-  password: process.env.PASSWORD_DATABASE,
-  database: process.env.DATABASE_NAME,
-  define: {
-    timestamps: true,
-    underscored: true
-  }
+    dialect: `${process.env.DIALECT_DATABASE}`,
+    host: `${process.env.HOST_DATABASE}`,
+    username: `${process.env.USERNAME_DATABASE}`,
+    password: `${process.env.PASSWORD_DATABASE}`,
+    database: `${process.env.DATABASE_NAME}`,
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
 };
